d9d18166c3ec280dbf38315cfa163d1d2a0dc608
JRPino <pino3789@gmail.com>
2020-02-28 19:35:56 -0700

    Added into the README and modified js

---
+++ README.md
@@ -0,0 +1,25 @@
+# Homework-3 JavaScript Password Generator
+
+# Introduction
+This project was to create an application that generates a random password based on user-selected criteria. This application runs in the browswer and features an HTML and CSS that is powered by JavaScript code.
+
+## Requirements
+ * To code JavaScript and make it function with the HTML and CSS
+
+### Design
+  * index.html - added a Favicon.
+  * style.css - was provided and I did no modification to it.
+  * script.js - to make it function with the HTML and CSS inorder for the application to generate.
+
+### Project Acceptance Criteria
+   User must click button in order to be prompted to answer a few questions about length and password criteria. Once user has selected the length and password criteria it will be validated. After the validation process a password will be generated into the <textarea> of the HTML that matches the selected criteria. 
+
+
+## Acknowledgement
+  * W3Schools https://www.w3schools.com/
+  * MDN Web Docs https://developer.mozilla.org/en-US/docs/Web/JavaScript
+  * The Ultimate Favicon Generator https://favicon.io/
+
+
+## Site
+https://jpino7.github.io/Password-Generator/
--- script.js
+++ script.js
@@ -1,68 +1,67 @@
 // These are variables that link to the html
 var generateBtn = document.querySelector("#generate")
-  // alert(generateBtn); ----this allows me to know button functions
+// alert(generateBtn); ----this allows me to know button functions
 var passwordText = document.querySelector("#password");
 
 
-//empty array for to store password characters
+//Empty array for PW characters
 var pwArray = [];
 
-// event listener to generate PW button when clicked
+// Event Listener to generate PW button when clicked
 generateBtn.addEventListener("click", buttonClicked);
 
 
-// Promts for Password Length
-function buttonClicked(){
-var pwLength = prompt("How long would you like your password? Choose between 8-128 characters");
-if(pwLength < 8 || pwLength > 128) {
-  alert("Please choose between 8-128");
-  return buttonClicked();
-}
+// Promts User for Password Length
+function buttonClicked() {
+  var pwLength = prompt("How long would you like your password? Choose between 8-128 characters");
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Please choose between 8-128");
+    return buttonClicked();
+  }
 
-// This will confirm if user wants uppercase letters 
-var upperCase = confirm("Would you like to include an Uppercase letter in your password?");
-var lowerCase = confirm("Would you like to include an Lowercase letter in your password?");
-var numbers = confirm("How about any numbers?");
-var symbols = confirm("What about any symbols?");
-
-
-
-// Arrays for random PW 
-var upper = ["A", "B", "C", "D", "E", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y" ,"Z"];
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
-
-// push arrays for whatever user selects in criteria
-var randomize = [];
+  // This will confirm if user wants uppercase letters 
+  var upperCase = confirm("Would you like to include an Uppercase letter in your password?");
+  var lowerCase = confirm("Would you like to include an Lowercase letter in your password?");
+  var numbers = confirm("How about any numbers?");
+  var symbols = confirm("What about any symbols?");
 
 
-if(upperCase){
-  randomize.push(...upper);
-}
 
-// This will confirm if user wants lowercase letters 
-if(lowerCase){
-  randomize.push(...lower);
-} 
+  // Arrays for random PW 
+  var upper = ["A", "B", "C", "D", "E", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var sym = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"];
 
-// This will confirm if user wants numbers 
-if(numbers){
-  randomize.push(...num);
-} 
+  // Push arrays for user PW criteria
+  var randomize = [];
 
-// This will confirm if user wants symbols 
-if(symbols){
-  randomize.push(...sym);
-} 
+  // Confirming if user wants uppercase letters
+  if (upperCase) {
+    randomize.push(upper);
+  }
 
-  // for loop goes here
-  for (var i = 0; i < pwLength; i++){
-    pwArray.push(randomize[Math.floor(Math.random() * randomize.length -1)])
+  // Confirming if user wants lowercase letters
+  if (lowerCase) {
+    randomize.push(...lower);
+  }
+
+  // Confirming if user wants numbers
+  if (numbers) {
+    randomize.push(...num);
+  }
+
+  // Confirming if user wants symbols
+  if (symbols) {
+    randomize.push(...sym);
+  }
+
+  // For loop goes here
+  for (var i = 0; i < pwLength; i++) {
+    pwArray.push(randomize[Math.floor(Math.random() * randomize.length - 1)])
     var convertString = pwArray.join("");
   }
 
-  // Push function into HTML to make Generated PW to be seen
-    passwordText.textContent = convertString;
-
+  // Push function into HTML to make Generated PW to be seen in textarea
+  passwordText.textContent = convertString;
 };
